6dd61b95aabdb2af0dea63a3f6af7c4bb086c548
ColeBranston <118498961+ColeBranston@users.noreply.github.com>
2025-02-22 02:13:07 -0500

    Added error handling on the batch script

--- count.bat
+++ count.bat
@@ -2,23 +2,24 @@
 setlocal EnableDelayedExpansion
 
 set target_directory=C:\Users\Administrator\OneDrive\Desktop\BookingScript\BookingScript
-
 cd /d %target_directory%
 
 set counter_file=counter.txt
 
 if not exist %counter_file% (
-	echo 0 > %counter_file%
+    echo 0 > %counter_file%
 )
 
 set /p counter=<%counter_file%
-
 set /a counter+=1
 
 echo %counter% > %counter_file%
-
 echo Counter value: %counter%
 
-git add .
-git commit -m "Making Changes"
-git push
+git add . 2>> error.log
+git commit -m "Making Changes" 2>> error.log
+git push 2>> error.log
+
+if %errorlevel% neq 0 (
+    echo An error occurred. Check error.log for more details.
+)
