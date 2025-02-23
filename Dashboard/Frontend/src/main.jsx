1998ad2115f7127af2ef8fba50869f9b70f81940
ColeBranston <118498961+ColeBranston@users.noreply.github.com>
2025-02-23 04:39:26 -0500

    Making Changes

--- Dashboard/Frontend/src/main.jsx
+++ Dashboard/Frontend/src/main.jsx
@@ -2,12 +2,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom' 
 import LoginForm from './Pages/LoginForm'
+import Dashboard from './Pages/Dashboard'
+import {LoginProvider} from './contexts/isLoggedIn'
+
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-    <Routes>
-        <Route path='/' element={<LoginForm/>}/>
-    </Routes>
-    </BrowserRouter>
-
+    <LoginProvider>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<LoginForm/>}/>
+                <Route path='/Dashboard' element={<Dashboard/>}/>
+            </Routes>
+        </BrowserRouter>
+    </LoginProvider>
 )
