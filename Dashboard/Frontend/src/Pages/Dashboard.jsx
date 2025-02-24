c7ea3016e84c7ff7896eab5e56e5c002a3c597b5
ColeBranston <118498961+ColeBranston@users.noreply.github.com>
2025-02-23 20:31:24 -0500

    highlights current day on dashboard

--- Dashboard/Frontend/src/Pages/Dashboard.jsx
+++ Dashboard/Frontend/src/Pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { checkLogin } from '../contexts/isLoggedIn';
+import moment from 'moment'
 
 const Dashboard = () => {
   const { isLoggedIn } = useContext(checkLogin);
@@ -30,14 +31,23 @@ const Dashboard = () => {
     fetchEntries(); // Call the fetchEntries function when the component mounts
   }, []);
 
+  const currentDate = moment().format('YYYY-MM-DD'); // Get the current date in YYYY-MM-DD format
+  console.log(currentDate)
   return (
     <>
       {isLoggedIn ? null : <Navigate to="/" />}
       <div className="dashboard-container">
         <h1>Dashboard</h1>
         <div className="entries-container">
-          {entries.map((entry, index) => (
-            <div className="entry-card" key={entry._id} style={{ animationDelay: `${index * 0.1}s` }}>
+          {entries.slice().reverse().map((entry, index) => (
+            <div
+              className="entry-card"
+              key={entry._id}
+              style={{
+                animationDelay: `${index * 0.1}s`,
+                border: entry.Date == currentDate ? '3px solid red' : 'none'
+              }}
+            >
               <h2>{entry.Room}</h2>
               <p>Date: {entry.Date}</p>
               <p>Time: {entry.Time}</p>
