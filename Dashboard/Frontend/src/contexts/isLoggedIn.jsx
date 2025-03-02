3bb5a2155a5edaa146d63c9900105f52e8727162
ColeBranston <118498961+ColeBranston@users.noreply.github.com>
2025-03-01 20:22:40 -0500

    added password caching

--- Dashboard/Frontend/src/contexts/isLoggedIn.jsx
+++ Dashboard/Frontend/src/contexts/isLoggedIn.jsx
@@ -3,8 +3,9 @@ import { createContext, useState, useContext} from 'react'
 export const checkLogin = createContext();
 
 export const LoginProvider = ({children}) => {
+    //Use state fields modified through the application
     const [isLoggedIn, setIsLoggedIn] = useState(false)
-
+    
     return (
         <checkLogin.Provider value={{isLoggedIn, setIsLoggedIn}}>
         {children}
