666e2e0376f4310109354c7fb216211595504bb2
ColeBranston <118498961+ColeBranston@users.noreply.github.com>
2025-03-02 08:29:08 -0500

    added to cors

--- Dashboard/Backend/server.js
+++ Dashboard/Backend/server.js
@@ -19,7 +19,17 @@ const corsOptions = {
 
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Handle preflight OPTIONS requests
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 
 
 
