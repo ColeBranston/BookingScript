1a657504ee9804bc8410cc833dd9890c34b6d003
ColeBranston <118498961+ColeBranston@users.noreply.github.com>
2025-03-04 00:47:28 -0500

    finalized working web hook and fully deployed frontend and backend

--- Script/Backend/script_server.py
+++ Script/Backend/script_server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def post_entry():
     data = request.json
     try:
-        response = requests.post('http://localhost:4000/api/create/entry', json=data)
+        response = requests.post('https://bookingbackend.vercel.app/api/create/entry', json=data)
         response.raise_for_status()  # Check for HTTP errors
         return jsonify(response.json()), response.status_code
     except requests.exceptions.RequestException as e:
