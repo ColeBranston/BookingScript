bc03de62db3aab6f63eb503d4f03aa865d9c91b7
Cole Branston <colebranston@gmail.com>
2025-03-15 00:28:51 -0400

    added feature branch

--- Dashboard/Frontend/src/main.jsx
+++ Dashboard/Frontend/src/main.jsx
@@ -3,16 +3,18 @@ import './index.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom' 
 import LoginForm from './Pages/LoginForm'
 import Dashboard from './Pages/Dashboard'
-import {LoginProvider} from './contexts/isLoggedIn'
+import ProtectedRoute from './JWT/ProtectedRoute';
+
 
 
 createRoot(document.getElementById('root')).render(
-    <LoginProvider>
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<LoginForm/>}/>
-                <Route path='/Dashboard' element={<Dashboard/>}/>
+                <Route
+                path="/Dashboard"
+                element={<ProtectedRoute element={<Dashboard />} />}
+                />
             </Routes>
         </BrowserRouter>
-    </LoginProvider>
 )
