97895b2452001d0756015d9d109ad6be32dd41a2
Cole Branston <colebranston@gmail.com>
2025-03-24 00:10:59 -0400

    got access tokens working, look at YOU repo for working refresh tokens

--- Dashboard/Frontend/src/main.jsx
+++ Dashboard/Frontend/src/main.jsx
@@ -4,10 +4,12 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import LoginForm from './Pages/LoginForm'
 import Dashboard from './Pages/Dashboard'
 import ProtectedRoute from './JWT/ProtectedRoute';
+import { AuthProvider } from './contexts/isLoggedIn'; // Ensure this path is correct
 
 
 
 createRoot(document.getElementById('root')).render(
+    <AuthProvider>
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<LoginForm/>}/>
@@ -17,4 +19,5 @@ createRoot(document.getElementById('root')).render(
                 />
             </Routes>
         </BrowserRouter>
+    </AuthProvider>
 )
